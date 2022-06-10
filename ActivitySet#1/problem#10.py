a2208243c4827963a860ed83127a3be045ec4907
purplesnow31 <94883115+purplesnow31@users.noreply.github.com>
2022-06-10 03:10:50 +0000

    done

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,2 +1,21 @@
 # Dictionaries
 filename = "dataset/mbox-short.txt"
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+counts = {}
+for line in handle:
+    word = line.split()
+    if len(word) < 3 or word[0] != "From" : continue
+    email = word[1]
+    if email in counts :
+        counts[email] = 1 + counts[email]
+    else :
+        counts.update({email:1})
+max = 0
+value = ""
+for i in counts:
+    if (counts[i]) > max :
+        max = counts[i]
+        value = i
+print(value, max)
